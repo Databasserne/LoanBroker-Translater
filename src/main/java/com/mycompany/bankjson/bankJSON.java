050a4c072360eb08006d4f5d53b36c7e8b017323
Jonas Simonsen <jks.ufo@icloud.com>
2017-10-05 09:07:31 +0200

    fedactor name

--- src/main/java/com/mycompany/bankjson/bankJSON.java
+++ src/main/java/com/mycompany/bankjson/bankJSON.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.bankJSON;
+package com.mycompany.bankjson;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
